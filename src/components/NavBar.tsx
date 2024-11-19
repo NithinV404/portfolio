0e95ceb36c386eea66ddb88882b3b89d9ab6735c
NithinV404 <nithin.v053@gmail.com>
2024-11-19 13:41:10 +0530

    Updated dependencies and remove personal info

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,16 @@
 import "../App.scss"
+
+
 function NavBar()
 {
+
+
     return(
-        <nav>
+        <nav id='nav'> 
         <div className="menu">
             <p className="logo">PORTFOLIO</p>
         <div className="menu-items">
-            <a href="#about" className="link">ABOUT</a>
+            <a href="#abou" id='abt' className="link">ABOUT </a>
             <a href="#projects" className="link">PROJECTS</a>
             <a href="#footer" className="link">CONTACTS</a>
         </div>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,5 @@
 import '../App.scss'
+
 function Profile()
 {
     return(<>
